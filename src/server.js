9f8c2bec0229032e3fe6960cc5c96208c4630a19
meryeemdifangel <jm_dif@esi.dz>
2023-05-12 11:58:05 +0100

    hihi

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const errorMiddleware = require("./middleware/error");
 // Route Imports
-const routes = require("./routes/index");
+//const routes = require("./routes/index");
 
 
 if(!dotenv.config().error)
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-app.use("/api/v1", routes);
+//app.use("/api/v1", routes);
 
 // Middleware for Errors
 app.use(errorMiddleware);
