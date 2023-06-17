c3a2d66247bb91470bbd5641a48733be293e8779
meryeemdifangel <jm_dif@esi.dz>
2023-06-17 23:02:32 +0100

    updates

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -16,7 +16,7 @@ const getAllOrdersOfClient = async (req , res) => {
 
 const addOrder = async (req , res ) => {
     try {
-        const order = await Order.create({...req.body,client: req.user.id});
+        const order = await Order.create(req.body);
         res.status(200).send(order);
     } catch (error) {
         console.log(error)
